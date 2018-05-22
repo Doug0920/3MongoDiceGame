f6728a73027981df17178212fe4a5d51b038e1fe
doug0920 <doug0920@hotmail.com>
2018-05-22 15:33:26 -0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST to adduser. */
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var collection = db.get('playerlist');
+  collection.insert(req.body, function(err, result){
+    res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+  });
+});
+
+
+/* GET userlist. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+    var collection = db.get('playerlist');
+//debug
+    console.log('GET route on userlist.');
+    console.log(collection);
+
+   collection.find({},{},function(e,docs){
+     res.json(docs);
+   });
+ });
+
+ /* DELETE to deleteuser. */
+ router.delete('/deleteplayer/:id', function(req, res) {
+     var db = req.db;
+     var collection = db.get('playerlist');
+     var userToDelete = req.params.id;
+     collection.remove({ '_id' : userToDelete }, function(err) {
+       res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+     });
+   });
+
+
+//  router.???('/userlist', function(req, res) {
+//  users.update({name: 'foo'}, {name: 'bar'})
+
+
+
+module.exports = router;
